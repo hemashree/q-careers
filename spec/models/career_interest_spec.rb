1e1a0f005add6b20848e87f73ee8ef4a9970b99d
hemashree <hemashree34@yopmail.com>
2015-06-22 14:40:08 +0530

    fixed the rspec issues

--- spec/models/career_interest_spec.rb
+++ spec/models/career_interest_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 RSpec.describe CareerInterest, type: :model do
 
   # let(:career_interest) {FactoryGirl.create(:career_interest)}
-  # let(:candidate1) {FactoryGirl.create(:candidate, name: "Test user", email: "email1@domain.com", phone: "952-365-1245")}
-  # let(:candidate2) {FactoryGirl.create(:candidate, name: "Krishna Prasad", email: "email2@domain.com", phone: "952-365-1246")}
-  # let(:candidate3) {FactoryGirl.create(:candidate, name: "Anjankumar", email: "email3@domain.com", phone: "952-365-1247", current_city: "Mysore", native_city: "Mandya")}
-  # let(:event1) {FactoryGirl.create(:event, :name => "Event1", :venue => "Qwinix Mysore", :slug => "sjc")}
-  # let(:event2) {FactoryGirl.create(:event, :name => "Event2", :description => "Drive 2015", :slug => "vvc")}
-  # let(:event3) {FactoryGirl.create(:event, :name => "Event3", :slug => "vvk")}
-  # let(:career_interest1) {FactoryGirl.create(:career_interest, candidate_id: candidate1.id, event_id: event1.id )}
-  # let(:career_interest2) {FactoryGirl.create(:career_interest, candidate_id: candidate2.id, event_id: event2.id )}
-  # let(:career_interest3) {FactoryGirl.create(:career_interest, candidate_id: candidate3.id, event_id: event3.id, reported: true)}
-  # let(:user) {FactoryGirl.create(:user)}
+   let(:candidate1) {FactoryGirl.create(:fresher_candidate, name: "Test user", email: "email1@domain.com", phone: "9523651245")}
+   let(:candidate2) {FactoryGirl.create(:experienced_candidate, name: "Krishna Prasad", email: "email2@domain.com", phone: "9523651246")}
+   let(:candidate3) {FactoryGirl.create(:experienced_candidate, name: "Anjankumar", email: "email3@domain.com", phone: "9523651247", current_city: "Mysore", native_city: "Mandya")}
+   let(:event1) {FactoryGirl.create(:event, :name => "Event1", :venue => "Qwinix Mysore", :slug => "sjc")}
+   let(:event2) {FactoryGirl.create(:event, :name => "Event2", :description => "Drive 2015", :slug => "vvc")}
+   let(:event3) {FactoryGirl.create(:event, :name => "Event3", :slug => "vvk")}
+   let(:career_interest1) {FactoryGirl.create(:employee_open_referral, candidate_id: candidate1.id, event_id: event1.id )}
+   let(:career_interest2) {FactoryGirl.create(:employee_job_referral, candidate_id: candidate2.id, event_id: event2.id )}
+   let(:career_interest3) {FactoryGirl.create(:registration_desk_entry, candidate_id: candidate3.id, event_id: event3.id, reported: true)}
+   let(:user) {FactoryGirl.create(:user)}
 
   context "Factory" do
     it "should validate all the career_interest factories" do
@@ -54,14 +54,14 @@ RSpec.describe CareerInterest, type: :model do
   context "Instance Methods" do
     it "should generate application code" do
       event10 = FactoryGirl.create(:event, :id => 1, :name => "Event10", :venue => "Qwinix Mysore", :slug => "sjce")
-      candidate10 = FactoryGirl.create(:candidate, name: "Test user", email: "email1@domain.com", phone: "952-365-1245")
-      career_interest10 = FactoryGirl.create(:career_interest, :id => 1, :event_id => event10.id, :candidate_id => candidate10.id)
-      expect(career_interest10.application_id).to eq("1-RD101")
+      candidate10 = FactoryGirl.create(:fresher_candidate, name: "Test user", email: "email1@domain.com", phone: "9523651245")
+      career_interest10 = FactoryGirl.create(:employee_open_referral, :id => 1, :event_id => event10.id, :candidate_id => candidate10.id)
+      expect(career_interest10.application_id).to eq("1-ER101")
     end
 
     it "the career interests can confirm, report or cancel report" do
-      expect(career_interest.confirm!).to eq(career_interest.confirmed == true )
-      expect(career_interest.report!).to eq(career_interest.reported == true )
+      expect(career_interest1.confirm!).to eq(career_interest1.confirmed == true )
+      expect(career_interest1.report!).to eq(career_interest1.reported == true )
       expect(career_interest3.cancel_report!).to eq(career_interest3.reported == false )
     end
 
