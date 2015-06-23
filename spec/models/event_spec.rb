21d8b9b2234df046d893423518a647648730b88e
Krishnaprasad Varma <krshnaprsad@gmail.com>
2015-06-23 09:36:15 +0530

    Events Cleanup.

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 RSpec.describe Event, type: :model do
 
-  let(:event1) {FactoryGirl.create(:event, :name => "Event1", :venue => "Qwinix Mysore", :slug => "sjc")}
-  let(:event2) {FactoryGirl.create(:event, :name => "Event2", :description => "Drive 2015", :slug => "vvc")}
-  let(:event3) {FactoryGirl.create(:event, :name => "Event3", :slug => "vvk")}
+  let(:qwinix_careers) {FactoryGirl.create(:qwinix_careers, :name => "Qwinix Careers", slug: "qwinix-careers")}
+  let(:apple) {FactoryGirl.create(:planning_event, name: "Apple", venue: "Jammu", description: "Fruit - Apple a day keeps doctor away", slug: "sebu")}
+  let(:mango) {FactoryGirl.create(:scheduled_event, name: "Mango", venue: "Karataka", description: "Fruit - King of fruits", slug: "maavu")}
+  let(:orange) {FactoryGirl.create(:past_event, name: "Orange", venue: "Nagpur", description: "Fruit - Orange for healthy life", slug: "kithale")}
 
   context "Factory" do
     it "should validate all the event factories" do
-      expect(FactoryGirl.build(:event).valid?).to be true
+      expect(FactoryGirl.build(:planning_event).valid?).to be true
+      expect(FactoryGirl.build(:scheduled_event).valid?).to be true
+      expect(FactoryGirl.build(:past_event).valid?).to be true
     end
   end
 
@@ -17,59 +20,86 @@ RSpec.describe Event, type: :model do
   end
 
   context "Validations" do
-    it { should validate_presence_of :name }
-    it { should allow_value('Recruitment Drive 2015').for(:name )}
-    it { should validate_presence_of :slug }
-    it { should allow_value('walk-in').for(:slug )}
-    it { should validate_presence_of :date }
-    it { should allow_value('2015-02-14').for(:date )}
-    it { should validate_presence_of :status }
-    it { should allow_value('planning').for(:status )}
-    it { should allow_value('scheduled').for(:status )}
-    it { should allow_value('over').for(:status )}
-    it { should_not allow_value('status').for(:status )}
-    it { should validate_presence_of :venue }
-    it { should allow_value('Qwinix Technologies, Mysore').for(:venue )}
-    it { should_not allow_value('Mysore$%?').for(:venue )}
-    it { should_not allow_value("x"*2057).for(:description )}
-  end
+    it { 
+      should validate_presence_of :name
+      should allow_value('Recruitment Drive 2015').for(:name)
+    }
 
+    it { 
+      should validate_presence_of :slug
+      should allow_value('walk-in').for(:slug)
+    }
+
+    it { 
+      should validate_presence_of :date
+      should allow_value('2015-02-14').for(:date)
+    }
+
+    it { 
+      should validate_presence_of :status
+      should allow_value('planning').for(:status)
+      should allow_value('scheduled').for(:status)
+      should allow_value('over').for(:status)
+      should_not allow_value('status').for(:status)
+    }
+    
+    it { 
+      should validate_presence_of :venue
+      should allow_value('Qwinix Technologies, Mysore').for(:venue)
+      should_not allow_value('Mysore$%?').for(:venue)
+    }
+
+    it { should_not allow_value("x"*2057).for(:description) }
+  end
 
   context "Class Methods" do
     it "search" do
       arr = [event1, event2, event3]
-      expect(Event.search("Qwinix Mysore")).to match_array([event1])
-      expect(Event.search("Drive 2015")).to match_array([event2])
-      expect(Event.search("Event3")).to match_array([event3])
+      
+      # Search Name
+      expect(Event.search("Apple")).to match_array([apple])
+      expect(Event.search("Mango")).to match_array([mango])
+      expect(Event.search("Orange")).to match_array([orange])
+
+      # Search Venue
+      expect(Event.search("Jammu")).to match_array([apple])
+      expect(Event.search("Karataka")).to match_array([mango])
+      expect(Event.search("Nagpur")).to match_array([orange])
+
+      # Search Description
+      expect(Event.search("doctor")).to match_array([apple])
+      expect(Event.search("King")).to match_array([mango])
+      expect(Event.search("healthy")).to match_array([orange])
+
+      expect(Event.search("Fruit")).to match_array(arr)
+
+      # Search Slug
+      expect(Event.search("sebu")).to match_array([apple])
+      expect(Event.search("maavu")).to match_array([mango])
+      expect(Event.search("kithale")).to match_array([orange])
+
+      # Negative Case
+      expect(Event.search("Hello World")).to match_array([])
     end
 
     it "it shows upcoming events" do
-      event1 = FactoryGirl.create(:event, :name => "Event4", :slug => "up1", date: Date.tomorrow)
-      event2 = FactoryGirl.create(:event, :name => "Event5", :slug => "up2", date: Date.tomorrow)
-      event3 = FactoryGirl.create(:event, :name => "Event6", :slug => "up3", date: Date.tomorrow)
-      arr = [event1, event2, event3]
-     expect(Event.upcoming_events).to match_array(arr)
+      arr = [qwinix_careers, apple, mango, orange]
+      expect(Event.upcoming_events).to match_array([mango, orange])
     end
   end
 
   context "Instance Methods" do
     it "it should display status" do
-      event1 = FactoryGirl.build(:event, :name => "Event7", :slug => "up4", status: "scheduled")
-      event2 = FactoryGirl.build(:event, :name => "Event8", :slug => "up5", status: "planning")
-      event3 = FactoryGirl.build(:event, :name => "Event9", :slug => "up6", status: "over")
-      expect(event1.display_status).to eq("Scheduled")
-      expect(event2.display_status).to eq("Planning")
-      expect(event3.display_status).to eq("Over")
+      expect(apple.display_status).to eq("Planning")
+      expect(mango.display_status).to eq("Scheduled")
+      expect(orange.display_status).to eq("Over")
     end
 
     it "it should test the event is over or planning or it is scheduled" do
-      event1 = FactoryGirl.build(:event, :name => "Event10", :slug => "up7", status: "scheduled")
-      event2 = FactoryGirl.build(:event, :name => "Event11", :slug => "up8", status: "planning")
-      event3 = FactoryGirl.build(:event, :name => "Event12", :slug => "up9", status: "over")
       array = [event1, event2, event3]
-      expect(event3.over?).to eq(event3.status == "over")
-      expect(event1.scheduled?).to eq(event1.status == "scheduled")
-      expect(event2.planning?).to eq(event2.status == "planning")
+      expect(apple.planning?).to be_true
+      expect(mango.scheduled?).to be_true
+      expect(orange.over?).to be_true
     end
   end
 end
